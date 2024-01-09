65c3f936113f0da04d45c367c43ae42f08a11c3f
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-01-09 17:59:05 +0400

    initial commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Lika Nozadze on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
