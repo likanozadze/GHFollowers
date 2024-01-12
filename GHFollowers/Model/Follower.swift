947f17b1759bb4fff9377db27d613e186770f63d
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-01-13 00:49:08 +0400

    add models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Lika Nozadze on 1/13/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
